90f3248da68f5e696d0dac1f91cd49b70d2848f8
Heo, Changhun <powerhch@gmail.com>
2024-03-15 01:35:36 +0900

    최초 커밋

---
+++ src/main/java/net/skhu/FireProtectMApplication.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireProtectMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireProtectMApplication.class, args);
+	}
+
+}

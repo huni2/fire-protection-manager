b6043ce4015ce82feb122b81e15267182255560e
Heo, Changhun <powerhch@gmail.com>
2024-04-03 23:56:45 +0900

    로그인 화면 수정 #3

---
+++ src/main/java/net/skhu/controller/MainController.java
@@ -0,0 +1,31 @@
+package net.skhu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("main")
+    public String main(Model model) {
+
+        return "eq/mainpage";
+    }
+
+	@PostMapping("check")
+    public String check(Model model) {
+
+        return "redirect:eq/list";
+    }
+}
+
+//@GetMapping("create")
+//public String create(Model model, Pagination1 pagination) {
+//    EqEdit eqEdit = new EqEdit();
+//    List<User> users = userService.findAll();
+//    model.addAttribute("eqEdit", eqEdit);
+//    model.addAttribute("users", users);
+//    return "eq/edit";
+//}
